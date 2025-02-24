b05cb2c772817b627edc1b437cbbd390da4e5d9a
st4rri <st4rri@tutamail.com>
2025-02-24 15:58:54 +1100

    updated coloum position to I can have two termincal windows side-by-side without text going off-screen

--- lua/opts.lua
+++ lua/opts.lua
@@ -20,5 +20,5 @@ vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "64"
 vim.opt.updatetime = 50
